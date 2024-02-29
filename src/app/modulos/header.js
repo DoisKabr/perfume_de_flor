664987f43ed5050974ae997274854004d1cd0852
DoisKa <38229999+DoisKabr@users.noreply.github.com>
2024-02-29 18:03:07 -0300

    Add files via upload

--- src/app/modulos/header.js
+++ src/app/modulos/header.js
@@ -5,7 +5,7 @@ import NavBar from "./navBar.js"
 export default function Header() {
     return (
         <header className={style.headerStyle}>
-            <h1 className={style.titulo}>Sublime Personalizados</h1>
+            <h1 className={style.titulo}>Perfume de Flor</h1>
             <NavBar />
         </header>
     )
